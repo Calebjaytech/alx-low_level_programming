3708eb026841a86630c979582ea5d912b19d0988
Calebjaytech <calebtech4real@gmail.com>
2022-09-14 14:11:17 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,38 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints the alphabet in lowercase.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char letter;
+
+
+
+			for (letter = 'a'; letter <= 'z'; letter++)
+
+						putchar(letter);
+
+
+
+				putchar('\n');
+
+
+
+					return (0);
+
+}
