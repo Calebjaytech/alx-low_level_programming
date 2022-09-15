8338b4c46c5bfd96fb04b9bea3b5d2f07cc0fcab
Calebjaytech <calebtech4real@gmail.com>
2022-09-15 09:33:05 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,15 +2,10 @@
 #define MAIN_H
 
 /*
-
  * File: holberton.h
-
  * Auth: Brennan D Baraban
-
  * Desc: Header file containing prototypes for all functions
-
 * used in the 0x03-more_functions_nested_loops directory.
-
  */
 
 int _putchar(char c);
