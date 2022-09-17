363ad3ed8ae4d71bab76affb1ebefd6a091514de
Calebjaytech <calebtech4real@gmail.com>
2022-09-17 05:41:52 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,45 +1,20 @@
-*
-*
-*  * File: 2-print_alphabet.c
-*
-*   * Auth: Brennan D Baraban
-*
-*    */
-
-
-
-#include <stdio.h>
-
 #include "main.h"
-
 /**
- *
- *  * main - Prints the alphabet in lowercase.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
 
-int main(void)
+void print_alphabet_x10(void)
 
 {
+	int count = 0;
 
-		char letter;
-
-
-
-			for (letter = 'a'; letter <= 'z'; letter++)
-
-						putchar(letter);
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	char letter;
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
+Footer
+© 2022 GitHub, Inc.
