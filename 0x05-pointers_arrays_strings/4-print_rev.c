7f9f9caa3b3c0638798acb8b3c75a3cdc66561ba
Calebjaytech <calebtech4real@gmail.com>
2022-09-19 21:05:14 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,32 @@ void print_rev(char *s)
 
 {
 
-	int len = 0, index;
+	int len = 0, in string in reverse.                                                                                          
+		
+		  5  * @s: The string to be printed.                                                                                                    
+		  
+		    6  */                                                                                                                                 
+		    
+		      7                                                                                                                                     
+		      
+		        8 void print_rev(char *s)                                                                                                             
+			
+			  9                                                                                                                                     
+			  
+			   10 {                                                                                                                                   
+				   
+				    11                                                                                                                                     
+					    
+					     12         int len = 0, index;                                                                                                         
+				    
+				     13                                                                                                                                     
+					     
+					      14                                                                                                                                     
+					      
+					       15                                                                                                                                     
+					       
+					        16         while (s[index++])                                                                       :x
+
 
 
 
@@ -21,7 +46,10 @@ void print_rev(char *s)
 
 	for (index = len - 1; index >= 0; index--)
 
-		_putchar(s[index]);
+
+
+
+ndex]);
 
 
 
