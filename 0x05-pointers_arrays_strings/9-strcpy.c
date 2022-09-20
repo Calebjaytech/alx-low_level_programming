941159c425c67e681c2a82bdf477a8c9ae2c49a7
Calebjaytech <calebtech4real@gmail.com>
2022-09-20 12:43:11 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcpy - Copies a string pointed to by @src, including the
  *terminating null byte, to a buffer pointed to by @des
@@ -9,25 +8,13 @@
  */
 
 char *_strcpy(char *dest, const char *src)
-
 {
-
 	int index = 0;
 
-
-
 	while (src[index])
-
 	{
-
 		dest[index] = src[index];
-
 		index++;
-
 	}
-
-
-
 	return (dest);
-
 }
