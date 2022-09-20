a78c820127a85279891ac836b8b8fd1655f59ea3
Calebjaytech <calebtech4real@gmail.com>
2022-09-19 18:08:36 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+uth: Brennan D Baraban
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+
+void _puts(char *str)
+
+{
+
+	while (*str)
+
+		_putchar(*str++);
+
+
+
+	_putchar('\n');
+
+}
