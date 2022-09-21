cf8b7be1aded5c6a8c146e278a660942ba0b5976
Calebjaytech <calebtech4real@gmail.com>
2022-09-20 21:09:34 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,58 +1,16 @@
 #include "main.h"
-
 /**
  * print_rev - Prints a string in reverse.
  * @s: The string to be printed.
  */
 
 void print_rev(char *s)
-
 {
-
-	int len = 0, in string in reverse.                                                                                          
-		
-		  5  * @s: The string to be printed.                                                                                                    
-		  
-		    6  */                                                                                                                                 
-		    
-		      7                                                                                                                                     
-		      
-		        8 void print_rev(char *s)                                                                                                             
-			
-			  9                                                                                                                                     
-			  
-			   10 {                                                                                                                                   
-				   
-				    11                                                                                                                                     
-					    
-					     12         int len = 0, index;                                                                                                         
-				    
-				     13                                                                                                                                     
-					     
-					      14                                                                                                                                     
-					      
-					       15                                                                                                                                     
-					       
-					        16         while (s[index++])                                                                       :x
-
-
-
+	int len = 0, index;
 
 	while (s[index++])
-
 		len++;
-
-
-
 	for (index = len - 1; index >= 0; index--)
-
-
-
-
-ndex]);
-
-
-
+		_putchar(s[index]);
 	_putchar('\n');
-
 }
