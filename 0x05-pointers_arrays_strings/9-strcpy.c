a12b2d754a2c217b364960defbd8b593370f64ce
Calebjaytech <calebtech4real@gmail.com>
2022-09-20 21:32:17 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,10 @@
-/*
- * File: 9-strcpy.c
- * Auth: Brennan D Baraban
- */
 #include "main.h"
+
 /**
  * _strcpy - Copies a string pointed to by @src, including the
- *           terminating null byte, to a buffer pointed to by @dest.
+ * terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
- *
  * Return: A pointer to the destination string @dest.
  */
 
