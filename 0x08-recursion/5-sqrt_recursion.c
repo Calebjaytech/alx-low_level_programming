a7703c36ed7a7d4d347f05002e535df673846581
Calebjaytech <calebtech4real@gmail.com>
2022-10-15 14:57:07 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,6 +18,7 @@ int power_operation(int n, int c)
 	}
 	return (0 + power_operation(n, c + 1));
 }
+
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: input number.
@@ -33,4 +34,4 @@ int _sqrt_recursion(int n)
 	if (n == 1)
 		return (1);
 	return (power_operation(n, 2));
-
+}
