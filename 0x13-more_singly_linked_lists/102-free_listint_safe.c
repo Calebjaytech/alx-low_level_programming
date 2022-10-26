f12dff6ba482aa402fac57bd21c6de82c95e04e1
Calebjaytech <calebtech4real@gmail.com>
2022-10-25 20:51:23 -0700

    ok

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,18 +11,17 @@ size_t free_listint_safe(listint_t **h);
  * Return: If the list is not looped - 0.
  *         Otherwise - the number of unique nodes in the list.
  */
-
 size_t looped_listint_count(listint_t *head)
 {
 	listint_t *tortoise, *hare;
-
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
-
 		return (0);
+
 	tortoise = head->next;
 	hare = (head->next)->next;
+
 	while (hare)
 	{
 		if (tortoise == hare)
@@ -34,19 +33,24 @@ size_t looped_listint_count(listint_t *head)
 				tortoise = tortoise->next;
 				hare = hare->next;
 			}
+
 			tortoise = tortoise->next;
 			while (tortoise != hare)
 			{
 				nodes++;
 				tortoise = tortoise->next;
 			}
+
 			return (nodes);
 		}
+
 		tortoise = tortoise->next;
 		hare = (hare->next)->next;
 	}
+
 	return (0);
 }
+
 /**
  * free_listint_safe - Frees a listint_t list safely (ie.
  *                     can free lists containing loops)
@@ -63,15 +67,17 @@ size_t free_listint_safe(listint_t **h)
 	size_t nodes, index;
 
 	nodes = looped_listint_count(*h);
+
 	if (nodes == 0)
 	{
 		for (; h != NULL && *h != NULL; nodes++)
-	{
+		{
 			tmp = (*h)->next;
 			free(*h);
 			*h = tmp;
 		}
 	}
+
 	else
 	{
 		for (index = 0; index < nodes; index++)
@@ -80,8 +86,11 @@ size_t free_listint_safe(listint_t **h)
 			free(*h);
 			*h = tmp;
 		}
+
 		*h = NULL;
 	}
+
 	h = NULL;
+
 	return (nodes);
 }
