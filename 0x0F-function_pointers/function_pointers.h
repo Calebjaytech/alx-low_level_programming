37fab97a9ad2d2b0f10b95e2d4a600793c8ee227
Calebjaytech <calebtech4real@gmail.com>
2022-11-16 06:18:36 -0800

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,13 +3,15 @@
 
 /*
  * File: function_pointers.h
- * AUth: Oni Michael
+ * AUth: jacob caleb
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
+ * used in the 0x0E-function_pointers directory.
  */
 
 #include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
